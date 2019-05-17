be28fea7e168ad9dba249c87ff111dd5375d118c
Alexandr klochkov <surfer.ru@mail.ru>
2019-05-18 03:39:55 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/LotsOf.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class LotsOf<T> : IEnumerable
+    {
+        public string mess = "LotsOf";
+        private List<T> items = new List<T>();
+
+        public int Count => items.Count;
+
+        public LotsOf() { }
+
+        public LotsOf(T item)
+        {
+            items.Add(item);
+
+        }
+
+        public LotsOf(IEnumerable<T> items)
+        {
+            this.items = items.ToList();
+        }
+
+        public void Add(T item)
+        {
+            if (items.Contains(item))
+            {
+                return;
+            }
+
+            items.Add(item);
+        }
+
+        public void Remove(T item)
+        {
+            items.Remove(item);
+        }
+
+        public LotsOf<T> Union(LotsOf<T> set)//объединение
+        {
+            // return new LotsOf<T>(items.Union(set.items));
+
+            LotsOf<T> result = new LotsOf<T>();
+
+
+            foreach (var item in items)
+            {
+                result.Add(item);
+            }
+
+            foreach (var item in set.items)
+            {
+                result.Add(item);
+            }
+
+            return result;
+        }
+
+        public LotsOf<T> Intersection(LotsOf<T> set)//пересечение
+        {
+
+            //return new LotsOf<T>(items.Intersect(set.items));
+            var result = new LotsOf<T>();
+            LotsOf<T> big;
+            LotsOf<T> small;
+            if (Count >= set.Count)
+            {
+                big = this;
+                small = set;
+            }
+            else
+            {
+                big = set;
+                small = this;
+            }
+            foreach (var item1 in small.items)
+            {
+                foreach (var item2 in big.items)
+                {
+                    if (item1.Equals(item2))
+                    {
+                        result.Add(item1);
+                        break;
+                    }
+                }
+            }
+            return result;
+        }
+
+        public LotsOf<T> Difference(LotsOf<T> set)//разность
+        {
+            // return new LotsOf<T>(items.Except(set.items));
+            var result = new LotsOf<T>(items);
+
+            foreach (var item in set.items)
+            {
+                result.Remove(item);
+            }
+
+            return result;
+        }
+
+        public bool Subset(LotsOf<T> set)//подмножество
+        {
+            // return items.All(i => set.items.Contains(i));
+
+            foreach (var item1 in items)
+            {
+                var equals = false;
+                foreach (var item2 in set.items)
+                {
+                    if (item1.Equals(item2))
+                    {
+                        equals = true;
+                        break;
+                    }
+                }
+
+                if (equals == false)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public LotsOf<T> SummetricDifference(LotsOf<T> set)//семмитричная разность
+        {
+            // return new LotsOf<T>(items.Except(set.items).Union(set.items.Except(items)));
+
+            var result = new LotsOf<T>();
+
+            foreach (var item1 in items)
+            {
+                var equals = false;
+
+                foreach (var item2 in set.items)
+                {
+
+                    if (item1.Equals(item2))
+                    {
+                        equals = true;
+                        break;
+                    }
+
+                }
+
+                if (equals == false)
+                {
+                    result.Add(item1);
+                }
+            }
+
+            foreach (var item1 in set.items)
+            {
+                var equals = false;
+                foreach (var item2 in items)
+                {
+
+                    if (item1.Equals(item2))
+                    {
+                        equals = true;
+                        break;
+                    }
+
+                }
+
+                if (equals == false)
+                {
+                    result.Add(item1);
+                }
+            }
+
+            return result;
+        }
+
+
+        public IEnumerator GetEnumerator()
+        {
+            return items.GetEnumerator();
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number = 0;
+
+            bool start = false;
+
+
+
+            do
+            {
+                Console.WriteLine("Реализация Абстрактных типов данных и операций над ними. Множества \r\n");
+                Console.WriteLine("Выберите режим работы программы:");
+                Console.WriteLine("1: Интерактивный: данные вводятся в консоль с клавиатуры, результаты так же выводятся на консоль;");
+                Console.WriteLine("2: Полуинтерактивный: включается, если при запуске программы обнаруживается файл input.txt, в котором находятся исходные (входные) данные, результаты выводятся так же на консоль;");
+                Console.WriteLine("3: Неинтерактивный: включается, если при запуске программы обнаруживается файл data.txt, в котором находятся исходные (входные) данные, результаты выводятся в файл output.txt. Окно программы не показывается;");
+                Console.WriteLine("4: Exit");
+                number = Convert.ToInt32(Console.ReadLine());
+
+                switch (number)
+                {
+                    case 1:
+                        Console.WriteLine("Вы выбрали Интерактивный вариант реализации\r\n");
+
+                        int nA,nB,nC;
+                        int[] A;
+                        int[] B;
+                        int[] C;
+                        int currint;
+
+                        do
+                        {
+
+                            Console.Clear();
+                            Console.Write("Введите размерность множества A: ");
+
+                        } while (!int.TryParse(Console.ReadLine(), out currint));
+
+                        nA = currint;
+                        A = new int[nA];
+
+                        for (int i = 0; i < nA; i++)
+                        {
+
+                            Console.Write("Введите элемент {0}: ", i + 1);
+
+                            if (int.TryParse(Console.ReadLine(), out currint))
+                            {
+                                A[i] = currint;
+                            }
+                            else --i;
+                        }
+
+                        do
+                        {
+
+                            Console.Clear();
+                            Console.Write("Введите размерность миножества B: ");
+
+                        } while (!int.TryParse(Console.ReadLine(), out currint));
+
+                        nB = currint;
+                        B = new int[nA];
+
+                        for (int j = 0; j < nA; j++)
+                        {
+
+                            Console.Write("Введите элемент {0}: ", j + 1);
+
+                            if (int.TryParse(Console.ReadLine(), out currint))
+                            {
+                                B[j] = currint;
+                            }
+                            else --j;
+                        }
+
+                        do
+                        {
+
+                            Console.Clear();
+                            Console.Write("Введите размерность миножества C: ");
+
+                        } while (!int.TryParse(Console.ReadLine(), out currint));
+
+                        nC = currint;
+                        C = new int[nC];
+
+                        for (int k = 0; k < nC; k++)
+                        {
+
+                            Console.Write("Введите элемент {0}: ", k + 1);
+
+                            if (int.TryParse(Console.ReadLine(), out currint))
+                            {
+                                C[k] = currint;
+                            }
+                            else --k;
+                        }
+
+                        var easyset1 = new LotsOf<int>(A);
+                        var easyset2 = new LotsOf<int>(B);
+                        var easyset3 = new LotsOf<int>(C);
+
+                        //var easyset1 = new LotsOf<int>(new int[] {1,2,3,4,5});
+                        //var easyset2 = new LotsOf<int>(new int[] {4,5,6,7,8 });
+                        //var easyset3 = new LotsOf<int>(new int[] {3,4,5});
+
+                        Console.Write("Union - операция объединения: ");
+                        foreach (var item in easyset1.Union(easyset2))
+                        {
+                            Console.Write(item + " ");
+                        }
+                        Console.WriteLine("\r\n");
+
+                        Console.Write("Intersection - операция пересечения: ");
+                        foreach (var item in easyset1.Intersection(easyset2))
+                        {
+                            Console.Write(item + " ");
+                        }
+                        Console.WriteLine("\r\n");
+
+                        Console.Write("Difference - операция разности A \\ B: ");
+                        foreach (var item in easyset1.Difference(easyset2))
+                        {
+                            Console.Write(item + " ");
+                        }
+                        Console.WriteLine("\r\n");
+
+                        Console.Write("Difference - операция разности B \\ A: ");
+                        foreach (var item in easyset2.Difference(easyset1))
+                        {
+                            Console.Write(item + " ");
+                        }
+                        Console.WriteLine("\r\n");
+
+                        Console.Write("Subset - операция определения подмножества A Subset C: ");
+                        Console.Write(easyset1.Subset(easyset3));
+                        Console.WriteLine("\r\n");
+
+                        Console.Write("Subset - операция определения подмножества C Subset A: ");
+                        Console.Write(easyset3.Subset(easyset1));
+                        Console.WriteLine("\r\n");
+
+                        Console.Write("Subset - операция определения подмножества C Subset B: ");
+                        Console.Write(easyset3.Subset(easyset2));
+                        Console.WriteLine("\r\n");
+
+                        Console.Write("SummetricDifference - операция семметричной разности: ");
+                        foreach (var item in easyset1.SummetricDifference(easyset2))
+                        {
+                            Console.Write(item + " ");
+                        }
+                        Console.WriteLine("\r\n");
+
+
+
+                  
+                        break;
+                    case 2:
+                        Console.WriteLine("Вы выбрали Полуинтерактивный вариант реализации");
+                        break;
+                    case 3:
+                        Console.WriteLine("Вы выбрали Неинтерактивный вариант реализации");
+                        break;
+                    case 4:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Вы ввели неверное значение повторите попытку или закройте  приложение\r\n");
+                        start = true;
+                        break;
+                }
+
+            }while (start);
+
+
+                Console.ReadKey();
+           
+        }
+    }
+}
